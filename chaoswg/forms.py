1c7af58f9fabb5edfc559660d742825d3fbdefb0
Markus Hilger <obihoernchende@gmail.com>
2018-02-07 23:33:56 +0100

    Add some defaults for form input

--- chaoswg/forms.py
+++ chaoswg/forms.py
@@ -12,8 +12,12 @@ class LoginForm(FlaskForm):
 
 class CreateTaskForm(FlaskForm):
     task = StringField(u'Task', validators=[InputRequired()])
-    base_points = IntegerField(u'Base Points', validators=[NumberRange(1, 13, 'Value must be between 1 and 13')])
-    time_factor = FloatField(u'Time Factor', validators=[NumberRange(0.0, 3.0, 'Value must be between 0.0 and 3.0')])
+    base_points = IntegerField(u'Base Points',
+                               validators=[InputRequired(), NumberRange(1, 13, 'Value must be between 1 and 13')],
+                               default=1)
+    time_factor = FloatField(u'Time Factor',
+                             validators=[InputRequired(), NumberRange(0.0, 3.0, 'Value must be between 0.0 and 3.0')],
+                             default=0.0)
     schedule_days = IntegerField(u'Schedule every X days (optional)',
                                  validators=[Optional(), NumberRange(1, 365, 'Value must be between 1 and 365')])
 
@@ -22,6 +26,7 @@ class CreateTaskForm(FlaskForm):
 
 class CustomTaskForm(FlaskForm):
     task = StringField(u'Custom Task', validators=[InputRequired()])
-    points = IntegerField(u'Points', validators=[NumberRange(1, 13, 'Value must be between 1 and 13')])
+    points = IntegerField(u'Points', validators=[InputRequired(), NumberRange(1, 13, 'Value must be between 1 and 13')],
+                          default=1)
 
     submit = SubmitField(u'Do Task now')
